27e97f71d455884fc087166e64d28daaec829b74
Arun Kumar <arunzone10@yahoo.com>
2014-10-08 15:48:38 +0530

    Nuera
    html

---
+++ js/header.php
@@ -0,0 +1,265 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Header Template
+ *
+ *
+ * @file           header.php
+ * @package        Responsive
+ * @author         Emil Uzelac
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.3
+ * @filesource     wp-content/themes/responsive/header.php
+ * @link           http://codex.wordpress.org/Theme_Development#Document_Head_.28header.php.29
+ * @since          available since Release 1.0
+ */
+?>
+	<!doctype html>
+	<!--[if !IE]>
+	<html class="no-js non-ie" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if IE 7 ]>
+	<html class="no-js ie7" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if IE 8 ]>
+	<html class="no-js ie8" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if IE 9 ]>
+	<html class="no-js ie9" <?php language_attributes(); ?>> <![endif]-->
+	<!--[if gt IE 9]><!-->
+<html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+	<head>
+
+
+
+
+
+		<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title><?php wp_title( '&#124;', true, 'right' ); ?></title>
+
+		<link rel="profile" href="http://gmpg.org/xfn/11"/>
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
+
+		<?php wp_head(); ?>
+		
+		
+		
+		
+		
+		
+		
+		<script src="<?php bloginfo('stylesheet_directory'); ?>/jquery-latest.js" type="text/javascript"></script>
+
+<link href="<?php bloginfo('stylesheet_directory'); ?>/bootstrap.css" rel="stylesheet" media="screen">
+<link href="<?php bloginfo('stylesheet_directory'); ?>/bootstrap-responsive.css" rel="stylesheet" media="screen">
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700' rel='stylesheet' type='text/css'>
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+
+
+<script src="<?php bloginfo('stylesheet_directory'); ?>/modernizr.custom.js"></script>
+
+
+	<link rel="stylesheet" href="css/lightbox.css" media="screen"/>
+
+<!-- html5.js for IE less than 9 -->
+<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<!-- css3-mediaqueries.js for IE less than 9 -->
+<!--[if lt IE 9]>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<![endif]-->
+
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/jquery.js"></script>
+
+<script>
+$(document).ready(function() {
+
+	//rotation speed and timer
+	var speed = 5000;
+	var run = setInterval('rotate()', speed);	
+	
+	//grab the width and calculate left value
+	var item_width = $('#slides li').outerWidth(); 
+	var left_value = item_width * (-1); 
+        
+    //move the last item before first item, just in case user click prev button
+	$('#slides li:first').before($('#slides li:last'));
+	
+	//set the default item to the correct position 
+	$('#slides ul').css({'left' : left_value});
+
+    //if user clicked on prev button
+	$('#prev').click(function() {
+
+		//get the right position            
+		var left_indent = parseInt($('#slides ul').css('left')) + item_width;
+
+		//slide the item            
+		$('#slides ul:not(:animated)').animate({'left' : left_indent}, 1000,function(){    
+
+            //move the last item and put it as first item            	
+			$('#slides li:first').before($('#slides li:last'));           
+
+			//set the default item to correct position
+			$('#slides ul').css({'left' : left_value});
+		
+		});
+
+		//cancel the link behavior            
+		return false;
+            
+	});
+
+ 
+    //if user clicked on next button
+	$('#next').click(function() {
+		
+		//get the right position
+		var left_indent = parseInt($('#slides ul').css('left')) - item_width;
+		
+		//slide the item
+		$('#slides ul:not(:animated)').animate({'left' : left_indent}, 1000, function () {
+            
+            //move the first item and put it as last item
+			$('#slides li:last').after($('#slides li:first'));                 	
+			
+			//set the default item to correct position
+			$('#slides ul').css({'left' : left_value});
+		
+		});
+		         
+		//cancel the link behavior
+		return false;
+		
+	});        
+	
+	//if mouse hover, pause the auto rotation, otherwise rotate it
+	$('#slides').hover(
+		
+		function() {
+			clearInterval(run);
+		}, 
+		function() {
+			run = setInterval('rotate()', speed);	
+		}
+	); 
+        
+});
+
+//a simple function to click next link
+//a timer will call this function, and the rotation will begin :)  
+function rotate() {
+	$('#next').click();
+}
+        
+        
+        
+</script>
+	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/jquery.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/script.js"></script>
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	</head>
+
+<body <?php body_class(); ?>>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php responsive_container(); // before container hook ?>
+<div id="container" class="hfeed">
+
+<?php responsive_header(); // before header hook ?>
+	<div id="header">
+
+		<?php responsive_header_top(); // before header content hook ?>
+
+		<?php if( has_nav_menu( 'top-menu', 'responsive' ) ) { ?>
+			<?php wp_nav_menu( array(
+								   'container'      => '',
+								   'fallback_cb'    => false,
+								   'menu_class'     => 'top-menu',
+								   'theme_location' => 'top-menu'
+							   )
+			);
+			?>
+		<?php } ?>
+
+		<?php responsive_in_header(); // header hook ?>
+
+		<?php if( get_header_image() != '' ) : ?>
+
+			<div id="logo">
+				<a href="<?php echo home_url( '/' ); ?>"><img src="<?php header_image(); ?>" width="<?php if( function_exists( 'get_custom_header' ) ) {
+						echo get_custom_header()->width;
+					}
+					else {
+						echo HEADER_IMAGE_WIDTH;
+					} ?>" height="<?php if( function_exists( 'get_custom_header' ) ) {
+						echo get_custom_header()->height;
+					}
+					else {
+						echo HEADER_IMAGE_HEIGHT;
+					} ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
+			</div><!-- end of #logo -->
+
+		<?php endif; // header image was removed ?>
+
+		<?php if( !get_header_image() ) : ?>
+
+			<div id="logo">
+				<span class="site-name"><a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
+				<span class="site-description"><?php bloginfo( 'description' ); ?></span>
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			</div><!-- end of #logo -->
+
+	
+
+	</div><!-- end of #header -->
+	
+	
+	
+
